77558568b5ae198f6e5a6dfaf05ba9b4b4486281
Ramkantodey <ramkantodey@gmail.com>
2024-10-02 15:04:33 +0600

    first commit

---
+++ async.js
@@ -0,0 +1,34 @@
+// console.log(1);
+// console.log(2);
+// setTimeout(() => {
+//     console.log('lazy logged');
+// }, 4000)
+// console.log(4);
+// console.log(5);
+// console.log(6);
+
+// const { reject } = require("async")
+
+
+// function doSomething() {
+//     console.log(3);
+// }
+
+const myLoader = () => {
+    return new Promise((resolve, reject) => {
+        const success = Math.random();
+        if (success <= 0.5) {
+            resolve(success)
+        }
+        else {
+            reject(success);
+        }
+    })
+}
+
+myLoader()
+
+
+    .then(data => console.log('resolved', data))
+    .catch(error => console.error('rejected with value ', error)
+    )
