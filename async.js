961ff7ea728fdd78042c69b85bda05927ecb6cbb
Ramkantodey <ramkantodey@gmail.com>
2024-10-02 15:42:53 +0600

    set interval done

--- async.js
+++ async.js
@@ -32,3 +32,22 @@ myLoader()
     .then(data => console.log('resolved', data))
     .catch(error => console.error('rejected with value ', error)
     )
+
+
+
+async function loadData() {
+    const res = await fetch('');
+    const data = await res.json()
+    console.log(data);
+}
+
+loadData();
+
+
+
+const taskLoader = async () => {
+    const res = await fetch('');
+    const data = await res.json();
+    console.log(data);
+
+}
